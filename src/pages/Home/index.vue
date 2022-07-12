9161ea196396471c9218d82cf7bba2333a622eb4
GISTWJ <3238670575@qq.com>
2022-07-12 22:09:45 +0800

    Create index.vue

---
+++ src/pages/Home/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-container style="height:100%">
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default { 
+    name:'home'
+};
+</script>
+
+<style lang="less" scoped> 
+ .el-header{ 
+     background-color: #333;
+ } 
+ .el-main{ 
+     padding: 0px;
+ }
+</style>
