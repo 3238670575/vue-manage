b6dbeb83c6957306068e7938ce0ae8c614fef6f0
GISTWJ <3238670575@qq.com>
2022-07-15 20:04:53 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import Vue from 'vue'
 import App from './App.vue' 
 import ElementUI from 'element-ui' 
 import 'element-ui/lib/theme-chalk/index.css' 
- 
 // 引入路由 
-import router from '@/router'
+import router from '@/router';
+// //引入全局组件 
+// import Navmenu from './pages/Home/Navmenu';
 
 Vue.config.productionTip = false 
 
 Vue.use(ElementUI); 
+// //注册全局组件：（组件名称，哪一个组件）
+// Vue.component(Navmenu.name,Navmenu); 
 
 new Vue({
   render: h => h(App),  
