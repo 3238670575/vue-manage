dd5588030390b079c9a9e5c6eebde597059bb68e
GISTWJ <3238670575@qq.com>
2022-07-15 20:05:00 +0800

    Create index.vue

---
+++ src/pages/Mains/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-container style="height: 100%">
+    <el-aside width="auto">
+      <Navmenu></Navmenu>
+    </el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main> 
+        <router-view></router-view> 
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script> 
+import Navmenu from '@/components/Navmenu' 
+
+export default {
+  name: "mains", 
+  components:{ 
+    Navmenu, 
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.el-header {
+  background-color: #333;
+}
+.el-main {
+  padding: 0px;
+}
+</style>
