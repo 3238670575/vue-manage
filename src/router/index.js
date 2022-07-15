7390335cfc8ba6bc18dfcaf8ee61842cfc67c083
GISTWJ <3238670575@qq.com>
2022-07-15 20:05:04 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,34 @@
-//配置路由 
-import VueRouter from "vue-router" 
-import Vue from 'vue' 
- 
-//使用路由插件 
-Vue.use(VueRouter) 
- 
-//引入路由组件 
-import Home from '@/pages/Home'
+//配置路由
+import VueRouter from "vue-router";
+import Vue from "vue";
 
-// 配置路由 
-export default new VueRouter({ 
-    routes:[ 
-        { 
-            path:'/home', 
-            component:Home,
-        }, 
-        { 
-            //重定向，初始界面 
-            path:'*', 
-            redirect:"/home"
-        } 
+//使用路由插件
+Vue.use(VueRouter);
 
-    ]
-})
+//引入路由组件
+import Home from "@/pages/Home";
+import Mains from "@/pages/Mains";
+
+// 配置路由
+export default new VueRouter({
+  routes: [
+    {
+      path: "/mains",
+      name: 'Mains',
+      component: Mains,
+    //   路由嵌套
+      children: [
+        {
+          path: "/home",
+          name:'home',
+          component: Home,
+        },
+      ],
+    },
+    {
+      //重定向，初始界面
+      path: "*",
+      redirect: "/home",
+    },
+  ],
+});
