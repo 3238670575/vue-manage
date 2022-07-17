5cd41c9ea2aebbe8ba58511c230c858f3e3e53b1
GISTWJ <3238670575@qq.com>
2022-07-17 13:55:19 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue' 
 import ElementUI from 'element-ui' 
-import 'element-ui/lib/theme-chalk/index.css' 
+import 'element-ui/lib/theme-chalk/index.css'  
+import './assets/less/index.less'
 // 引入路由 
-import router from '@/router';
+import router from '@/router'; 
+//引入仓库 
+import store from '@/store'
 // //引入全局组件 
 // import Navmenu from './pages/Home/Navmenu';
 
@@ -16,5 +19,7 @@ Vue.use(ElementUI);
 new Vue({
   render: h => h(App),  
   // 注册路由
-  router,
+  router,  
+  //注册仓库
+  store,
 }).$mount('#app')
