e2e81ad28796db8ee5ad0f1b5035ae44f820b3f2
GISTWJ <3238670575@qq.com>
2022-07-17 13:55:16 +0800

    Update index.vue

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,13 +1,84 @@
 <template>
-  <p>我是home</p>
+  <!-- 用户信息卡 -->
+  <el-row class="home" :gutter="20">
+    <el-col :span="8" style="margin-top: 20px">
+      <el-card shadow="hover">
+        <div class="user">
+          <img :src="userImg" />
+          <div class="userinfo">
+            <p class="name">Admin</p>
+            <p class="access">超级管理员</p>
+          </div>
+        </div>
+        <div class="login-info">
+          <p>上次登录时间:<span>2022-7-16</span></p>
+          <p>上次登录地点:<span>长沙</span></p>
+        </div>
+      </el-card>
+      <!-- 购买统计表 -->
+      <el-card style="margin-top: 20px; height: 460px">
+        <el-table :data="tableData"> 
+          <el-table-column v-for="(val,key) in tableLabel" :key="key" :prop="key" :label="val"> 
+          </el-table-column>
+        </el-table>
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
 export default {
-
-}
+  name: "home",
+  data() {
+    return {
+      userImg: require("@/assets/images/user.png"),
+      tableData: [
+        {
+          name: "oppo",
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800,
+        },
+        {
+          name: "vivo",
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800,
+        },
+        {
+          name: "苹果",
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800,
+        },
+        {
+          name: "小米",
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800,
+        },
+        {
+          name: "三星",
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800,
+        },
+        {
+          name: "魅族",
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800,
+        },
+      ], 
+      tableLabel:{ 
+        name:'品牌', 
+        todayBuy:'今日购买',  
+        monthBuy:'本月购买',
+        totalBuy:'总购买',
+      }
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
