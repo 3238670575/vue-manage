f3dd985abab6e3394cf64e8e84585f87f6e0a01b
GISTWJ <3238670575@qq.com>
2022-07-17 17:23:52 +0800

    Update index.vue

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -17,16 +17,57 @@
       </el-card>
       <!-- 购买统计表 -->
       <el-card style="margin-top: 20px; height: 460px">
-        <el-table :data="tableData"> 
-          <el-table-column v-for="(val,key) in tableLabel" :key="key" :prop="key" :label="val"> 
+        <el-table :data="tableData">
+          <el-table-column
+            v-for="(val, key) in tableLabel"
+            :key="key"
+            :prop="key"
+            :label="val"
+          >
           </el-table-column>
         </el-table>
       </el-card>
+    </el-col>
+    <el-col :span="16" style="margin-top: 20px">
+      <div class="num">
+        <el-card
+          v-for="item in countData"
+          :key="item.name"
+          :body-style="{ display: 'flex', padding: 0 }"
+        >
+          <i
+            class="icon"
+            :class="`el-icon-${item.icon}`"
+            :style="{ background: item.color }"
+          ></i>
+          <div class="detail">
+            <p class="num">￥{{ item.value }}</p>
+            <p class="txt">{{ item.name }}</p>
+          </div>
+        </el-card>
+      </div>
+      <el-card style="height: 280px">
+        <!-- <div style="height: 280px" ref="lineEcharts"></div> -->
+        <Echart :chartData="echartData.order" style="height: 280px"></Echart>
+      </el-card>
+      <div class="graph">
+        <el-card style="height: 260px">
+          <!-- <div style="height: 240px" ref="userEcharts"></div> --> 
+          <Echart :chartData="echartData.user" style="height: 240px"></Echart>
+        </el-card>
+        <el-card style="height: 260px"> 
+          <Echart :chartData="echartData.video" :isAxisChart="false" style="height: 240px"></Echart>
+          <!-- <div style="height: 240px" ref="videoEcharts"></div> -->
+        </el-card>
+      </div>
     </el-col>
   </el-row>
 </template>
 
 <script>
+import { getData } from "@/api/data.js";
+// import * as echarts from "echarts";
+import Echart from "../../components/Echarts/index.vue";
 export default {
   name: "home",
   data() {
@@ -69,14 +110,205 @@ export default {
           monthBuy: 300,
           totalBuy: 800,
         },
-      ], 
-      tableLabel:{ 
-        name:'品牌', 
-        todayBuy:'今日购买',  
-        monthBuy:'本月购买',
-        totalBuy:'总购买',
-      }
+      ],
+      tableLabel: {
+        name: "品牌",
+        todayBuy: "今日购买",
+        monthBuy: "本月购买",
+        totalBuy: "总购买",
+      },
+      countData: [
+        {
+          name: "今日支付订单",
+          value: 1234,
+          icon: "success",
+          color: "#2ec7c9",
+        },
+        {
+          name: "今日收藏订单",
+          value: 210,
+          icon: "star-on",
+          color: "#ffb980",
+        },
+        {
+          name: "今日未支付订单",
+          value: 1234,
+          icon: "s-goods",
+          color: "#5ab1ef",
+        },
+        {
+          name: "本月支付订单",
+          value: 1234,
+          icon: "success",
+          color: "#2ec7c9",
+        },
+        {
+          name: "本月收藏订单",
+          value: 210,
+          icon: "star-on",
+          color: "#ffb980",
+        },
+        {
+          name: "本月未支付订单",
+          value: 1234,
+          icon: "s-goods",
+          color: "#5ab1ef",
+        },
+      ],
+      echartData: {
+        order: {
+          xData: [],
+          series: [],
+        },
+        user: {
+          xData: [],
+          series: [],
+        },
+        video: {
+          series: [],
+        },
+      },
     };
+  },
+  components: {
+    Echart,
+  },
+  mounted() {
+    getData().then((res) => {
+      const { code, data } = res.data;
+
+      if (code === 20000) {
+        this.tableData = data.tableData;
+        const order = data.orderData;
+        const xData = order.date;
+        const keyArray = Object.keys(order.data[0]);
+        const series = [];
+
+        //折线图
+        keyArray.forEach((key) => {
+          // 配置series
+          series.push({
+            name: key,
+            data: order.data.map((item) => item[key]),
+            type: "line",
+          });
+        });
+
+        // const lineOption = {
+        //   xAxis: {
+        //     data: xData,
+        //   },
+        //   yAxis: {},
+        //   legend: {
+        //     data: keyArray,
+        //   },
+        //   series,
+        // };
+        this.echartData.order.xData = xData;
+        this.echartData.order.series = series;
+        // const lineEchart = echarts.init(this.$refs.lineEcharts);
+        // lineEchart.setOption(lineOption);
+
+        //用户柱状图
+        // const userOption = {
+        //   legend: {
+        //     // 图例文字颜色
+        //     textStyle: {
+        //       color: "#333",
+        //     },
+        //   },
+        //   grid: {
+        //     left: "20%",
+        //   },
+        //   // 提示框
+        //   tooltip: {
+        //     trigger: "axis",
+        //   },
+        //   xAxis: {
+        //     type: "category", // 类目轴
+        //     data: data.userData.map((item) => item.date),
+        //     axisLine: {
+        //       lineStyle: {
+        //         color: "#17b3a3",
+        //       },
+        //     },
+        //     axisLabel: {
+        //       interval: 0,
+        //       color: "#333",
+        //     },
+        //   },
+        //   yAxis: [
+        //     {
+        //       type: "value",
+        //       axisLine: {
+        //         lineStyle: {
+        //           color: "#17b3a3",
+        //         },
+        //       },
+        //     },
+        //   ],
+        //   color: ["#2ec7c9", "#b6a2de"],
+        //   series: [
+        //     {
+        //       name: "新增用户",
+        //       data: data.userData.map((item) => item.new),
+        //       type: "bar",
+        //     },
+        //     {
+        //       name: "活跃用户",
+        //       data: data.userData.map((item) => item.active),
+        //       type: "bar",
+        //     },
+        //   ],
+        // };
+        this.echartData.user.xData =  data.userData.map((item) => item.date);
+        this.echartData.user.series = [
+            {
+              name: "新增用户",
+              data: data.userData.map((item) => item.new),
+              type: "bar",
+            },
+            {
+              name: "活跃用户",
+              data: data.userData.map((item) => item.active),
+              type: "bar",
+            },
+          ]
+        // const userEchart = echarts.init(this.$refs.userEcharts);
+        // userEchart.setOption(userOption);
+
+        //饼图
+        // const videoOption = {
+        //   tooltip: {
+        //     trigger: "item",
+        //   },
+        //   color: [
+        //     "#0f78f4",
+        //     "#dd536b",
+        //     "#9462e5",
+        //     "#a6a6a6",
+        //     "#e1bb22",
+        //     "#39c362",
+        //     "#3ed1cf",
+        //   ],
+        //   series: [
+        //     {
+        //       data: data.videoData,
+        //       type: "pie",
+        //     },
+        //   ],
+        // };   
+        this.echartData.video.series =  [
+            {
+              data: data.videoData,
+              type: "pie",
+            },
+          ]
+        // const videoEchart = echarts.init(this.$refs.videoEcharts);
+        // videoEchart.setOption(videoOption);
+      }
+      console.log(res);
+    });
   },
 };
 </script>
