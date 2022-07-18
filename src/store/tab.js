10620696b0fc93c6c4ab350447d7e2971fbbd7bd
GISTWJ <3238670575@qq.com>
2022-07-18 21:37:26 +0800

    Update tab.js

--- src/store/tab.js
+++ src/store/tab.js
@@ -1,11 +1,32 @@
 //state :仓库存储数据的地方 
 const state={ 
-    isCollapse:false
+    isCollapse:false,  
+    // 面包屑初始数据
+    tabsList:[ 
+        { 
+            path:'/', 
+            name:'home', 
+            label:'首页', 
+            icon:'home'
+        }
+    ], 
+    currentMeun:null,
 }; 
 //mutations :修改state的唯一手段 
 const mutations={   
     COLLAPSEMEUN(state){ 
         state.isCollapse = !state.isCollapse
+    }, 
+    SELECTMEUN(state,val){ 
+        if(val.name !== 'home'){ 
+            state.currentMeun = val 
+            const result= state.tabsList.findIndex(item => item.name === val.name) 
+            if(result === -1){ 
+                state.tabsList.push(val)
+            }else{ 
+                state.currentMeun=null
+            }
+        }
     }
 };  
 // action:处理action，可以书写自己的业务逻辑，也可以处理异步 
