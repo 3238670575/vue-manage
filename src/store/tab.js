913a9794894d94e2635aa2b8ba5eccd3f29968d5
GISTWJ <3238670575@qq.com>
2022-07-31 15:48:22 +0800

    Update tab.js

--- src/store/tab.js
+++ src/store/tab.js
@@ -1,44 +1,44 @@
-//state :仓库存储数据的地方 
-const state={ 
-    isCollapse:false,  
-    // 面包屑初始数据
-    tabsList:[ 
-        { 
-            path:'/', 
-            name:'home', 
-            label:'首页', 
-            icon:'home'
-        }
-    ], 
-    currentMeun:null,
-}; 
-//mutations :修改state的唯一手段 
-const mutations={   
-    COLLAPSEMEUN(state){ 
-        state.isCollapse = !state.isCollapse
-    }, 
-    SELECTMEUN(state,val){ 
-        if(val.name !== 'home'){ 
-            state.currentMeun = val 
-            const result= state.tabsList.findIndex(item => item.name === val.name) 
-            if(result === -1){ 
-                state.tabsList.push(val)
-            }else{ 
-                state.currentMeun=null
-            }
-        }
+//state :仓库存储数据的地方
+const state = {
+  isCollapse: false,
+  // 面包屑初始数据
+  tabsList: [
+    {
+      path: "/",
+      name: "home",
+      label: "首页",
+      icon: "home",
+    },
+  ],
+  currentMeun: null,
+};
+//mutations :修改state的唯一手段
+const mutations = {
+  COLLAPSEMEUN(state) {
+    state.isCollapse = !state.isCollapse;
+  },
+  SELECTMEUN(state, val) {
+    if (val.name !== "home") {
+      state.currentMeun = val;
+      const result = state.tabsList.findIndex((item) => item.name === val.name);
+      if (result === -1) {
+        state.tabsList.push(val);
+      } else {
+        state.currentMeun = null;
+      }
     }
-};  
-// action:处理action，可以书写自己的业务逻辑，也可以处理异步 
-const actions={ 
-
+  },
+  CLOSETAG(state, val) {
+    const result = state.tabsList.findIndex((item) => item.name === val.name);
+    state.tabsList.splice(result, 1);
+  },
 };
-const getters={ 
-    
+// action:处理action，可以书写自己的业务逻辑，也可以处理异步
+const actions = {};
+const getters = {};
+export default {
+  state,
+  mutations,
+  actions,
+  getters,
 };
-export default{  
-    state, 
-    mutations, 
-    actions, 
-    getters,
-}
