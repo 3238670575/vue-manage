f980ed468a92469ce7e9440a726f7f5d102d0acc
GISTWJ <3238670575@qq.com>
2022-08-01 14:56:09 +0800

    Update mock.js

--- src/api/mock.js
+++ src/api/mock.js
@@ -1,6 +1,7 @@
 import Mock from "mockjs";
 import homeApi from "./Mock/home";
 import userApi from "./Mock/user";
+import permissionApi from './Mock/permission'
 
 Mock.mock("/home/getData", homeApi.getStatisticalData());
 
@@ -8,3 +9,4 @@ Mock.mock(/user\/add/, "post", userApi.createUser);
 Mock.mock(/user\/edit/, "post", userApi.updateUser);
 Mock.mock(/user\/getUser/, "get", userApi.getUserList);
 Mock.mock(/user\/del/, "post", userApi.deleteUser);
+Mock.mock(/permission\/getMenu/,'post',permissionApi.getMenu)
